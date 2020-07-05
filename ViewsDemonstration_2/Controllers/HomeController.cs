6e2bef9e4b5951037dd9e57eaa22e1bb8eaf4da2
Utpal Maiti <Utpalmaiti12@gmail.com>
2020-07-05 08:30:35 +0530

    Add project files.

---
+++ ViewsDemonstration_2/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ViewsDemonstration_2.Models;
+
+namespace ViewsDemonstration_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<Employee> empList = new List<Employee>
+            {
+                new Employee {Id=101,Name="Abhinav",Location="Bangalore",Salary=12345 },
+                new Employee {Id=102,Name="Abhishek",Location="Chennai",Salary=23456 },
+                new Employee {Id=103,Name="Akash",Location="Bangalore",Salary=34567 },
+                new Employee {Id=104,Name="Akshay",Location="Chennai",Salary=45678 },
+                new Employee {Id=105,Name="Anirudh",Location="Bangalore",Salary=56789 }
+            };
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View("Index", "~/Views/Shared/_BasicLayout.cshtml", empList);
+            //return View("Index", "_BasicLayout", empList);
+            //it is search first controller Views folder then Shared folder
+        }
+
+        public ActionResult EditEmployee(int Id)
+        {
+            var employee = empList.First(e => e.Id == Id);
+
+            return View(employee);
+        }
+
+        public ActionResult ManageUser()
+        {
+            //var employee = empList.First(e => e.Id == Id);
+
+            return View();
+        }
+    }
+}
---
+++ ViewsDemonstration_2/Global.asax.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ViewsDemonstration_2
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            Debug.WriteLine("Hello World!");
+
+            Thread thread = new Thread(InsertProcess);
+            Thread thread2 = new Thread(InsertProcess2);
+            thread.Start();
+            thread2.Start();
+
+            //Task task1 = Task.Run(async () =>  // <- marked async
+            //{
+            //    while (true)
+            //    {
+            //        Debug.WriteLine("Task.Delay(3000)!");
+
+            //        InsertProcess();
+            //        await Task.Delay(1000);
+            //    }
+            //});
+            //Task task2 = Task.Run(async () =>  // <- marked async
+            //{
+            //    while (true)
+            //    {
+            //        Debug.WriteLine("Task.Delay(10000)!");
+
+            //        InsertProcess();
+            //        await Task.Delay(10000);
+            //    }
+            //});
+
+            Debug.WriteLine("--------------");
+
+            Debug.WriteLine("---------------------------------!");
+
+            //Task.WaitAll(new[] { task1, task2 });
+        }
+
+        public static async void InsertProcess()
+        {
+            while (true)
+            {
+                Debug.WriteLine("InsertProcess!Delay(1000)");
+                await Task.Delay(1000);
+            }
+            //using (SqlConnection conn = new SqlConnection(RO1DBConn))
+            //{
+            //    // ... query database
+            //}
+        }
+
+        public static async void InsertProcess2()
+        {
+            while (true)
+            {
+                Debug.WriteLine("InsertProcess!");
+                await Task.Delay(5000);
+            }
+            //using (SqlConnection conn = new SqlConnection(RO1DBConn))
+            //{
+            //    // ... query database
+            //}
+        }
+    }
+}
